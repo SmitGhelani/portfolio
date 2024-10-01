7373adef3edea5b755e4ea9cd3a5463a1abb00ed
Smit Ghelani <ghelanismit4@gmail.com>
2024-10-01 09:30:22 +0530

    Next image added in place of img tag

--- app/components/profile/index.tsx
+++ app/components/profile/index.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 
 const Card = () => {
     return(
         <div className="justify-center items-center text-center">
             <div className="">
                 <div className="h-fit w-screen m-2">
-                    <img src="IMG_3854.jpg" className="m-auto rounded-full object-contain h-28 w-28 " alt="Profile-pic"/>
+                    <Image src="Image_3854.jpg" className=" m-auto rounded-full object-contain" width="28" height="28" alt="Profile-pic" />
                 </div> 
                 <div className="">
                     Hey I'm Smit Ghelani
@@ -38,38 +39,38 @@ const Card = () => {
                     <div><a href="https://www.linkedin.com/in/smitghelani/"><i className="fa-brands fa-linkedin"></i></a></div>
                     <div><a href="https://github.com/SmitGhelani"><i className="fa-brands fa-github"></i></a></div>
                     <div><a href="https://x.com/smitghelani06"><i className="fa-brands fa-x-twitter"></i></a></div>
-                    {/* <div><a href="https://leetcode.com/u/SmitGhelani/"><img src="leetcode.svg" className="h-6 w-6 items-center justify-center text-center"/></a></div> */}
+                    {/* <div><a href="https://leetcode.com/u/SmitGhelani/"><Image src="leetcode.svg" className="h-6 w-6 items-center justify-center text-center"/></a></div> */}
                 </div>
             </div>
             <div className="">
                 <div className="">Skills</div>
                 <div className="flex">
-                <img src="nodejs.svg" className="h-20 w-20"/>
-                <img src="aws.svg" className="h-20 w-20"/>
-                <img src="bash.svg" className="h-20 w-20"/>
-                <img src="ansible.svg" className="h-20 w-20"/>
-                <img src="css.svg" className="h-20 w-20"/>
-                <img src="docker.svg" className="h-20 w-20"/>
-                <img src="devops.svg" className="h-20 w-20"/>
-                <img src="express.svg" className="h-20 w-20"/>
-                <img src="gatsby.svg" className="h-20 w-20"/>
-                <img src="drupal.svg" className="h-20 w-20"/>
-                <img src="git.svg" className="h-20 w-20"/>
-                <img src="github.svg" className="h-20 w-20"/>
-                <img src="html.svg" className="h-20 w-20"/>
-                <img src="javascript.svg" className="h-20 w-20"/>
-                <img src="jenkins.svg" className="h-20 w-20"/>
-                <img src="linux.svg" className="h-20 w-20"/>
-                <img src="ML.svg" className="h-20 w-20"/>
-                <img src="mongo.svg" className="h-20 w-20"/>
-                <img src="mysql.svg" className="h-20 w-20"/>
-                <img src="next.svg" className="h-20 w-20"/>
-                <img src="php.svg" className="h-20 w-20"/>
-                <img src="python.svg" className="h-20 w-20"/>
-                <img src="react.svg" className="h-20 w-20"/>
-                <img src="tailwind.svg" className="h-20 w-20"/>
-                <img src="typescript.svg" className="h-20 w-20"/>
-                <img src="leetcode.svg" className="h-20 w-20"/>
+                <Image src="nodejs.svg" className="h-20 w-20" alt="" />
+                <Image src="aws.svg" className="h-20 w-20" alt=""/>
+                <Image src="bash.svg" className="h-20 w-20" alt=""/>
+                <Image src="ansible.svg" className="h-20 w-20" alt=""/>
+                <Image src="css.svg" className="h-20 w-20" alt=""/>
+                <Image src="docker.svg" className="h-20 w-20" alt=""/>
+                <Image src="devops.svg" className="h-20 w-20" alt=""/>
+                <Image src="express.svg" className="h-20 w-20" alt=""/>
+                <Image src="gatsby.svg" className="h-20 w-20" alt=""/>
+                <Image src="drupal.svg" className="h-20 w-20" alt=""/>
+                <Image src="git.svg" className="h-20 w-20" alt=""/>
+                <Image src="github.svg" className="h-20 w-20" alt=""/>
+                <Image src="html.svg" className="h-20 w-20" alt=""/>
+                <Image src="javascript.svg" className="h-20 w-20" alt=""/>
+                <Image src="jenkins.svg" className="h-20 w-20" alt=""/>
+                <Image src="linux.svg" className="h-20 w-20" alt=""/>
+                <Image src="ML.svg" className="h-20 w-20" alt=""/>
+                <Image src="mongo.svg" className="h-20 w-20" alt=""/>
+                <Image src="mysql.svg" className="h-20 w-20" alt=""/>
+                <Image src="next.svg" className="h-20 w-20" alt=""/>
+                <Image src="php.svg" className="h-20 w-20" alt=""/>
+                <Image src="python.svg" className="h-20 w-20" alt=""/>
+                <Image src="react.svg" className="h-20 w-20" alt=""/>
+                <Image src="tailwind.svg" className="h-20 w-20" alt=""/>
+                <Image src="typescript.svg" className="h-20 w-20" alt=""/>
+                <Image src="leetcode.svg" className="h-20 w-20" alt=""/>
                 </div>
             </div>
         </div>
