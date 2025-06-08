9aeab37d69ae0110715e6b10260baca18a4816aa
Smit Ghelani <ghelanismit4@gmail.com>
2025-06-08 14:08:26 +0530

    ADO-000: DNS change for porfolio site

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,7 +31,7 @@ const Project = () => {
           <h1 className="flex flex-row rounded-lg p-30 justify-center items-center text-center h-20 w-full bg-300% animate-gradient text-white font-cour text-2xl">My Projects</h1>      
         </div>
         <div className="flex flex-wrap rounded-lg basis-3/4  p-30 justify-center items-center text-center h-screen w-full bg-300% animate-gradient" >
-            <Link href="https://ecommerce.smitghelani.xyz/" className="text-white p-5 font-poppins object-fill w-[18rem] h-[20rem] items-center justify-center text-center m-2 rounded-xl hover:cursor-pointer" style={cardImgoneStyle} >
+            <Link href="https://ecommerce.smitghelani.site/" className="text-white p-5 font-poppins object-fill w-[18rem] h-[20rem] items-center justify-center text-center m-2 rounded-xl hover:cursor-pointer" style={cardImgoneStyle} >
               <div className="flex flex-row items-center justify-center text-center w-full">
                 <div className="flex basis-1/4 w-fit justify-start"><img src="store.png" className="h-10 w-10 rounded-xl" width="20" height="20" alt="" /></div>
                 <div className="flex basis-3/4">eCommerce Store</div>
@@ -39,7 +39,7 @@ const Project = () => {
               <div className="mt-10 justify-start text-left text-sm">NextJS based e-commerece store written with TypeScript. Well architectured redux store based cart and user state management.</div>
               <div className="mt-10 font-bebas justify-start text-left text-sm italic">NextJS | TypeScript | MongoDB | Postman</div>
             </Link>
-            <Link href="https://unionboard.smitghelani.xyz/" className="text-white p-5 font-poppins object-fill w-[18rem] h-[20rem] items-center justify-center text-center m-2 rounded-xl hover:cursor-pointer bg-[#19B79D]" style={cardImgtwoStyle} >
+            <Link href="https://unionboard.smitghelani.site/" className="text-white p-5 font-poppins object-fill w-[18rem] h-[20rem] items-center justify-center text-center m-2 rounded-xl hover:cursor-pointer bg-[#19B79D]" style={cardImgtwoStyle} >
               <div className="flex flex-row items-center justify-center text-center w-full">
                 <div className="flex basis-1/4 w-fit justify-start items-center text-center"><img src="logoPayment.png" className="h-10 w-10 rounded-xl" width="20" height="20" alt="" /></div>
                 <div className="flex basis-3/4 justify-start items-center text-center">UniOnBoard</div>
